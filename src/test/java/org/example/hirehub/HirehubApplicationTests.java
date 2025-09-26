4410e4eaa2766931adb9bac7e4b1c86ab96c76a0
unknown <ungquangtri@gmail.com>
2025-09-26 22:16:46 +0700

    first commit

---
+++ src/test/java/org/example/hirehub/HirehubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hirehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HirehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
