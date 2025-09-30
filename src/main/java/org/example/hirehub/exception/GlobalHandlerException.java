e15a4d6489302b4becdda05d316dbd9254745a49
unknown <ungquangtri@gmail.com>
2025-09-30 17:20:51 +0700

    Add global handler exception

--- src/main/java/org/example/hirehub/exception/GlobalHandlerException.java
+++ src/main/java/org/example/hirehub/exception/GlobalHandlerException.java
@@ -1,4 +1,18 @@
 package org.example.hirehub.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
 public class GlobalHandlerException {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, ?>> handleGlobalError(Exception e) {
+        e.printStackTrace();
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("message", e.getMessage()));
+    }
 }
