403891ac79b024e94c7859d0361686be39befd71
unknown <ungquangtri@gmail.com>
2025-09-30 17:17:28 +0700

    update cloudinary config and add  cloudinary service

--- src/main/java/org/example/hirehub/service/CloudinaryService.java
+++ src/main/java/org/example/hirehub/service/CloudinaryService.java
@@ -1,4 +1,25 @@
 package org.example.hirehub.service;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Service
 public class CloudinaryService {
+    private final Cloudinary cloudinary;
+
+    public CloudinaryService(Cloudinary cloudinary) {
+        this.cloudinary = cloudinary;
+    }
+
+    public String uploadAndGetUrl(MultipartFile file) throws IOException {
+        Map result = cloudinary.uploader()
+                .upload(file.getBytes(), ObjectUtils.emptyMap());
+
+        return (String) result.get("secure_url");
+     }
 }
