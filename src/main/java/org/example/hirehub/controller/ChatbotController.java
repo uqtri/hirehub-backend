5f29955533e03f75d34d04280f28c8779d45d135
uqtri <ungquangtri@gmail.com>
2025-11-30 14:41:11 +0700

    Add endpoints to analyze resume

--- src/main/java/org/example/hirehub/controller/ChatbotController.java
+++ src/main/java/org/example/hirehub/controller/ChatbotController.java
@@ -5,10 +5,7 @@ import org.example.hirehub.dto.job.JobDetailDTO;
 import org.example.hirehub.dto.job.JobSummaryDTO;
 import org.example.hirehub.service.ChatbotService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
@@ -30,5 +27,12 @@ public class ChatbotController {
         List<JobDetailDTO> jobs = chatbotService.findJob(request);
 
         return ResponseEntity.ok(Map.of("data", jobs));
+    }
+
+    @PostMapping("/resumes")
+    public ResponseEntity<Map<String, ?>> analyzeResume(@ModelAttribute ChatbotRequestDTO request) {
+
+        String response = chatbotService.analyzeResume(request);
+        return ResponseEntity.ok(Map.of("data", response));
     }
 }
