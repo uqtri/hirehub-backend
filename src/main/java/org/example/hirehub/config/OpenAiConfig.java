e2670dc0a38a0447d3173096a16d84f80aff777e
nngtkhngoc <khanhngoc230705@gmail.com>
2025-12-10 23:59:32 +0700

    resolve conflict made by uqt

--- src/main/java/org/example/hirehub/config/OpenAiConfig.java
+++ src/main/java/org/example/hirehub/config/OpenAiConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class OpenAiConfig {
 
-    @Value("${openai.api.key}")
+    @Value("${openai.api-key}")
     private String apiKey;
 
     @Value("${openai.base-url}")
